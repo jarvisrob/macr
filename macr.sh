0948e0096baa519aa634cc0fe653d89d61f4918d
Rob Jarvis <rob.jarvis@purple.telstra.com>
2020-12-30 13:43:36 +1100

    copied files from other projects, laid out steps in shell script comments

---
+++ macr.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+echo 'macr: Installing or upgrading R on macOS'
+echo 'Requires Homebrew: https://brew.sh/'
+
+
+# check if Homebrew installed, if not, then say needs to be installed and quit
+
+
+# check if R installed
+# check option--keep existing and install new, or remove existing then install
+
+# link BLAS
+
+# install packages
+# - done using R
+
+# install/configire IRkernel for Jupyter
+# - done using R
+
